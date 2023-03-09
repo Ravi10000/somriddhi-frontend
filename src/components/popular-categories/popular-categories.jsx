71efcbbcd40474811330f4e9680a4d77d33e9f29
Abhinav_007 <pabhinav400@gmail.com>
2023-03-09 12:33:49 +0530

    add popular categories table

--- src/components/popular-categories/popular-categories.jsx
+++ src/components/popular-categories/popular-categories.jsx
@@ -1,18 +1,29 @@
 import './popular-categories.styles.scss';
 
 import React from 'react'
+import categoriesList from './popular-categories-list';
 
 export default function PopulatCategories() {
-  return (
-    <section className='popular-categories-section'>
-        <div className="container">
-            <div className="left">
-                <img src="/table-bg.png" alt="categories table background" />
+    return (
+        <section className='popular-categories-section'>
+            <div className="container">
+                <div className="left">
+                    <img src="/table-bg.png" alt="categories table background" />
+                </div>
+                <div className="right">
+                    <h2 className='_title'>Popular Categories</h2>
+                    <div className="categories-table">
+                        {
+                            categoriesList.map((category, index) => (
+                                <div className="category-name" key={category}>
+                                    {category}
+                                </div>
+
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
-            <div className="right">
-                <h2 className='_title'>Popular Categories</h2>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
