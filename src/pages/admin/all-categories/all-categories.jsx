7f52084ae23ae0397011d28a4cee82b78217f749
ravi <pabhinav400@gmail.com>
2023-03-16 10:27:57 +0530

    add all-categories section for admin page

--- src/pages/admin/all-categories/all-categories.jsx
+++ src/pages/admin/all-categories/all-categories.jsx
@@ -2,6 +2,7 @@ import "./all-categories.styles.scss";
 
 import React from "react";
 import TitleSection from "../title-section/title-section";
+import categoryList from "./category-list";
 
 export default function AllCategories() {
   return (
@@ -12,6 +13,16 @@ export default function AllCategories() {
           alert("add categories popup need to be here");
         }}
       />
+      <div className="categories">
+        {categoryList?.map(({ name, img }, index) => (
+          <div className="category" key={index}>
+            <div className="category-icon">
+              <img src={img} alt={name} />
+            </div>
+            <p className="name">{name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/admin/all-memberships/all-memberships.jsx
+++ src/pages/admin/all-memberships/all-memberships.jsx
@@ -12,6 +12,36 @@ export default function AllMemberships() {
           alert("add memberships popup need to be here");
         }}
       />
+      <div className="membership-cards-container">
+        {/* {bannerList?.map(
+          ({ name, url, expiryDate, bannerImg, desc }, index) => ( */}
+        <div className="membership-card">
+          <img className="membership-img" src="/offer2.png" />
+          <div className="membership-details">
+            {/* <div className="info-container"> */}
+            <div className="membership-info">
+              <h5 className="name">Banner</h5>
+              <div className="info expiry-date">
+                <img src="/percentage.png" alt="percentage" />
+                <p>Cashback : 20%</p>
+              </div>
+              <div className="info membership-link">
+                <img src="/link.png" alt="membership link" />
+                <p>https//:url.com</p>
+              </div>
+            </div>
+            {/* <img className="lock-icon" src="/lock.png" alt="locked" /> */}
+            {/* </div> */}
+            <div className="membership-desc">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </div>
+          </div>
+        </div>
+        {/* )
+        )} */}
+      </div>
     </div>
   );
 }
