31318a7aa0209b59d403a590fde84390af1a0253
ravi <pabhinav400@gmail.com>
2023-03-16 11:11:12 +0530

    add all-memberships and all-newsletters section to admin page

--- src/pages/admin/all-newsletters/all-newsletters.jsx
+++ src/pages/admin/all-newsletters/all-newsletters.jsx
@@ -2,6 +2,7 @@ import "./all-newsletters.styles.scss";
 
 import React from "react";
 import TitleSection from "../title-section/title-section";
+import subscriptionsList from "./subscriptionsList";
 
 export default function AllNewsletters() {
   return (
@@ -12,6 +13,28 @@ export default function AllNewsletters() {
           alert("add newsletters popup need to be here");
         }}
       />
+      <table className="subscription-table">
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Added At</th>
+            <th>Total</th>
+            <th>Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          {subscriptionsList?.map(({ email, addedAt, total }, index) => (
+            <tr key={index}>
+              <td>{email}</td>
+              <td>{addedAt}</td>
+              <td>{total}</td>
+              <td>
+                <img src="/check.png" alt="" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
