9c51bfb1854603fd7e2dfcfa656c32e0e4ab568f
Arpita Saxena <arpitasaxena555@gmail.com>
2023-03-31 22:48:37 +0530

    latest dist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
         target: "http://13.234.49.6:8001",
       },
     },
+    https: {
+      key: '/etc/pki/tls/private/somriddhi_store.key',
+      cert: '/etc/pki/tls/certs/somriddhi_store.crt',
+      ca: '/etc/pki/tls/certs/somriddhi_store.ca-bundle',
+    }
   },
 });
