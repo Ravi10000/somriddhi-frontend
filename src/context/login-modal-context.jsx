c0dfba58bcc1bb96115a4266064328aa11fdb241
ravi <pabhinav400@gmail.com>
2023-04-04 18:06:09 +0530

    add redirect to home page on login popup close

--- src/context/login-modal-context.jsx
+++ src/context/login-modal-context.jsx
@@ -1,12 +1,16 @@
 import { useContext, createContext, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 export const LoginModalContext = createContext();
 
 export const useLoginModal = () => useContext(LoginModalContext);
 
 export default function LoginModalProvider({ children }) {
+  const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
-  const closeModal = () => setModalOpen(false);
+  const closeModal = () => {
+    setModalOpen(false);
+    navigate("/");
+  };
   const openModal = () => setModalOpen(true);
 
   return (
